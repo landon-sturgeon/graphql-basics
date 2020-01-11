642fe584a7cdb77425a393fe85f6f2e9866d1291
landon-sturgeon <sturgeon.landon@gmail.com>
2020-01-11 08:40:40 -0800

    added input type for mutatiions

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,5 +1,7 @@
 import { GraphQLServer } from "graphql-yoga";
 
+import uuidv4 from "uuid/v4";
+
 // Scalar Types
 // Strings, Booleans, Ints, Floats, ID
 
@@ -85,6 +87,32 @@ const typeDefs = `
         comments(query: String): [Comment!]!
     }
 
+    type Mutation {
+        createUser(data: CreateUserInput): User!
+        createPost(data: CreatePostInput): Post!
+        createComment(data: CreateCommentInput): Comment!
+    }
+
+    input CreateUserInput {
+        name: String!
+        email: String!
+        age: Int
+    }
+
+    input CreatePostInput {
+        title: String!
+        body: String!
+        published: Boolean!
+        author: ID!
+    }
+
+    input CreateCommentInput {
+        text: String!
+        published: Boolean!
+        author: ID!
+        post: ID!
+    }
+
     type User {
         id: ID!
         name: String!
@@ -140,6 +168,67 @@ const resolvers = {
             return demo_comments.filter((comment) => {
                 return comment.text.toLowerCase().includes(args.query.tolowercase());
             });
+        }
+    },
+    Mutation: {
+        createUser(parent, args, ctx, info) {
+            const emailTaken = demo_users.some((user) => {
+                return user.email === args.data.email
+            });
+
+            if (emailTaken) {
+                throw new Error("Email taken.");
+            }
+            
+            const user = {
+                id: uuidv4(),
+                ...args.data
+            }
+
+            demo_users.push(user);
+
+            return user;
+        },
+        createPost(parent, args, cts, info) {
+            const userExists = demo_users.some((user) => {
+                return user.id === args.data.author
+            });
+
+            if (!userExists) {
+                throw new Error("User not found");
+            };
+
+            const post = {
+                id: uuidv4(),
+                ...args.data
+            };
+
+            demo_posts.push(post);
+
+            return post;
+        },
+        createComment(parent, args, cts, info) {
+            const userExists = demo_users.some((user) => {
+                return user.id === args.data.author;
+            });
+            console.log(userExists);
+            const postExists = demo_posts.some((post) => {
+                return post.id === args.data.post && post.published;
+            });
+            console.log(postExists);
+
+            if (!userExists || !postExists) {
+                throw new Error("Unable to find user and post");
+            };
+
+            const comment = {
+                id: uuidv4(),
+                ...args.data
+            };
+
+            demo_comments.push(comment);
+
+            return comment;
         }
     },
     Post: {
