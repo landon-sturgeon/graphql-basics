026e67cfc0957344e234b32323099b8fea8d88b4
landon-sturgeon <sturgeon.landon@gmail.com>
2020-01-18 07:46:15 -0800

    added authentication for queries

--- graphql-prisma/src/resolvers/query.js
+++ graphql-prisma/src/resolvers/query.js
@@ -1,3 +1,5 @@
+import getUserId from "../utils/getUserId";
+
 const Query = {
     users(parent, args, { prisma }, info) {
         const opArgs = {};
@@ -14,20 +16,78 @@ const Query = {
 
         return prisma.query.users(opArgs, info);
     },
+    async me(parent, args, { prisma, request}, info) {
+        const userId = getUserId(request, false);
+        const users = await prisma.query.users({
+            where: {
+                id: userId
+            }
+        });
+
+        if (users.length === 0 ){
+            throw new Error("User not found");
+        };
+
+        return users[0]; 
+    },
     posts(parent, args, { prisma }, info) {
-        const opArgs = {};
+        const opArgs = {
+            where: {
+                published: true
+            }
+        };
 
         if (args.query) {
-            opArgs.where = {
-                OR: [{
-                    title_contains: args.query
-                }, {
-                    body_contains: args.query
-                }]
-            }
-        }
+            opArgs.where.OR = [{
+                title_contains: args.query
+            }, {
+                body_contains: args.query
+            }];
+        };
 
         return prisma.query.posts(opArgs, info);  
+    },
+    myPosts(parent, args, { prisma, request }, info) {
+        const userId = getUserId(request);
+        const opArgs = {
+            where: {
+                author: {
+                    id: userId
+                }
+            }
+        };
+
+        if (args.query) {
+            opArgs.where.OR = [{
+                title_contains: args.query
+            }, {
+                body_contains: args.query
+            }];
+        };
+
+        return prisma.query.posts(opArgs, info);
+    },
+    async post(parent, args, { prisma, request }, info) {
+        const userId = getUserId(request, false);
+
+        const posts = await prisma.query.posts({
+            where: {
+                id: args.id,
+                OR: [{
+                    published: true
+                }, {
+                    author: {
+                        id: userId
+                    }
+                }]
+            }
+        }, info);
+
+        if (posts.length === 0) {
+            throw new Error("Post not found");
+        }
+
+        return posts[0];
     },
     comments(parent, args, { prisma }, info) {
         const opArgs = {};
--- graphql-prisma/src/resolvers/users.js
+++ graphql-prisma/src/resolvers/users.js
@@ -1,5 +1,15 @@
-const User = {
+import getUserId from "../utils/getUserId";
 
+const User = {
+    email(parent, args, { request }, info) {
+        const userId = getUser(request, false);
+
+        if (userId && userId === parent.id) {
+            return parent.email;
+        } else {
+            return null;
+        };
+    }
 };
 
 export default User;
