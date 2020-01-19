cb1db8e4a48bd12aeaabfc0cd26a02428b092b96
landon-sturgeon <sturgeon.landon@gmail.com>
2020-01-19 12:10:48 -0800

    added tests for signup, posts, and users

--- graphql-prisma/tests/user.test.js
+++ graphql-prisma/tests/user.test.js
@@ -73,3 +73,82 @@ test("Should create a new user", async () => {
 
     expect(userExists).toBe(true);
 });
+
+test("Should expose public author profiles", async () => {
+    const getUsers = gql`
+        query {
+            users {
+                id
+                name
+                email
+            }
+        }
+    `;
+    const response = await client.query({ query: getUsers });
+
+    expect(response.data.users.length).toBe(1);
+    expect(response.data.users[0].email).toBe(null);
+    expect(response.data.users[0].name).toBe("jenn");
+}); 
+
+test("Should expose only published posts", async () => {
+    const getPosts = gql`
+        query {
+            posts {
+                title
+                body
+                published
+                author {
+                    name
+                    email
+                }
+            }
+        }
+    `;
+    const response = await client.query({ query: getPosts });
+
+    expect(response.data.posts.length).toBe(1);
+    expect(response.data.posts[0].title).toBe("example post 1");
+    expect(response.data.posts[0].body).toBe("prisma post body 1");
+    expect(response.data.posts[0].published).toBe(true);
+    expect(response.data.posts[0].author.name).toBe("jenn");
+    expect(response.data.posts[0].author.email).toBe(null);
+});
+
+test("Should not login with bad credentials", async () => {
+    const login = gql`
+        mutation {
+            login(
+                data: {
+                    email: "asdf@test.com",
+                    password: "asdfasdfp"
+                }
+            ){
+                token
+            }
+        }
+    `;
+
+    await expect(
+        client.mutate({ mutation: login })
+    ).rejects.toThrow();
+});
+
+test("Should not be able to signup with short password", async () => {
+    const signup = gql`
+        mutation {
+            createUser(
+                data: {
+                    name: "landon",
+                    email: "landon@example.com",
+                    password: "123"
+                }
+            ){
+                token
+            }
+        }
+    `;
+    await expect(
+        client.mutate({ mutation: signup })
+    ).rejects.toThrow();
+});
